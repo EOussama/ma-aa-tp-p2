5126853f547a50fdfcf8811e103f05399156454d
EOussama <oussama_gtacj@live.fr>
2023-11-14 00:11:22 +0100

    Added CRUD opts

--- src/polycreux.c
+++ src/polycreux.c
@@ -36,6 +36,23 @@ PolyCreux *GetLastMonome(PolyCreux *poly)
   return current;
 }
 
+PolyCreux *ConvertArray(PolyCreux *poly)
+{
+  int index = 0;
+  int count = CountMonomes(poly);
+
+  PolyCreux *monomesArray = (PolyCreux *)malloc(count * sizeof(PolyCreux));
+  PolyCreux *current = poly;
+
+  while (current != NULL)
+  {
+    monomesArray[index++] = *current;
+    current = current->PDroite;
+  }
+
+  return monomesArray;
+}
+
 void InsertMonome(PolyCreux *poly, int degre, int coefficient)
 {
   if (poly != NULL)
@@ -46,4 +63,108 @@ void InsertMonome(PolyCreux *poly, int degre, int coefficient)
     lastPoly->PDroite = newPoly;
     newPoly->PGauche = lastPoly;
   }
+}
+
+void DeleteMonome(PolyCreux *poly, int degre)
+{
+  PolyCreux *current = poly;
+  PolyCreux *previous = NULL;
+
+  while (current != NULL)
+  {
+    if (current->degre == degre)
+    {
+      if (previous == NULL)
+      {
+        poly = current->PDroite;
+      }
+      else
+      {
+        previous->PDroite = current->PDroite;
+      }
+
+      free(current);
+      current = current->PDroite;
+
+      continue;
+    }
+
+    previous = current;
+    current = current->PDroite;
+  }
+}
+
+void DestroyPolyCreux(PolyCreux *poly)
+{
+  PolyCreux *current = poly;
+  PolyCreux *next;
+
+  while (current != NULL)
+  {
+    next = current->PDroite;
+    free(current);
+    current = next;
+  }
+
+  free(next);
+}
+
+void PrintMonome(PolyCreux poly)
+{
+  printf("+ %dx^%d ", poly.coefficient, poly.degre);
+  printf("address: %p\n", poly.PDroite);
+}
+
+void PrintDesc(PolyCreux *poly)
+{
+  int count = CountMonomes(poly);
+  PolyCreux *monomialsArray = ConvertArray(poly);
+
+  qsort(monomialsArray, count, sizeof(PolyCreux), CompareDegreesDesc);
+
+  for (int i = 0; i < count; i++)
+  {
+    PrintMonome(monomialsArray[i]);
+  }
+
+  free(monomialsArray);
+}
+
+void PrintAsc(PolyCreux *poly)
+{
+  int count = CountMonomes(poly);
+  PolyCreux *monomialsArray = ConvertArray(poly);
+
+  qsort(monomialsArray, count, sizeof(PolyCreux), CompareDegreesAsc);
+
+  for (int i = 0; i < count; i++)
+  {
+    PrintMonome(monomialsArray[i]);
+  }
+
+  free(monomialsArray);
+}
+
+int CountMonomes(PolyCreux *poly)
+{
+  int count = 0;
+  PolyCreux *current = poly;
+
+  while (current != NULL)
+  {
+    count++;
+    current = current->PDroite;
+  }
+
+  return count;
+}
+
+int CompareDegreesDesc(const void *a, const void *b)
+{
+  return ((PolyCreux *)b)->degre - ((PolyCreux *)a)->degre;
+}
+
+int CompareDegreesAsc(const void *a, const void *b)
+{
+  return ((PolyCreux *)a)->degre - ((PolyCreux *)b)->degre;
 }
--- src/polycreux.h
+++ src/polycreux.h
@@ -13,6 +13,24 @@ PolyCreux *CreateMonome(int degre, int coefficient);
 
 PolyCreux *GetLastMonome(PolyCreux *poly);
 
+PolyCreux *ConvertArray(PolyCreux *poly);
+
 void InsertMonome(PolyCreux *poly, int degre, int coefficient);
+
+void DeleteMonome(PolyCreux *poly, int degre);
+
+void DestroyPolyCreux(PolyCreux *poly);
+
+void PrintMonome(PolyCreux poly);
+
+void PrintDesc(PolyCreux *poly);
+
+void PrintAsc(PolyCreux *poly);
+
+int CountMonomes(PolyCreux *poly);
+
+int CompareDegreesDesc(const void *a, const void *b);
+
+int CompareDegreesAsc(const void *a, const void *b);
 
 #endif
